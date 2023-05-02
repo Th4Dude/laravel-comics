3e8a70224e65edb16a83bf3f94ec1f9098bed53a
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-02 17:44:01 +0300

    add img + layout+ partials

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+
+<header>
+    <div class="container">
+        <div class="logo ">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="" srcset="">
+        </div>
+        <div class="menu">
+            <nav>
+                <ul class="list-unstyled d-flex justify-content-center gap-4">
+                 <li><a href="">Home</a></li>
+                 <li><a href="">Prodotti</a></li>
+                 <li><a href="">News</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
