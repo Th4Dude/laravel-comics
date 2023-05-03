e138188c1b82db6696852f1f5b315e5006da5a9c
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-03 20:36:37 +0300

    aggiunta parte di sotto locandina

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -12,18 +12,40 @@
         <h1 class="py-3">{{$comic['title']}}</h1>
         <nav class="navbar bg-body-tertiary bg-success">
             <div class="container-fluid">
-                <h1 class="fs-5">US PRICE :{{$comic['price']}}</h1>
+                <h1 class="fs-5">US PRICE : {{$comic['price']}}</h1>
             </div>
           </nav>
-        <p class="py-3">{{$comic['description']}}</p>
+        <p class="py-3"><em>{{$comic['description']}}</em></p>
     </div>
 
-    <div class="container-advertisement">
-        <h6>ADVERTISEMENT</h6>
-        <img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="" srcset="">
+    <div class="container-advertisement px-3 py-3">
+        <h6 class="d-flex justify-content-end  py-3" >ADVERTISEMENT</h6>
+        <img class="" src="{{ Vite::asset('resources/img/cioccio.jpg') }}" alt="" srcset="">
     </div>
 
 </div>
+
+<div class="d-flex justify-content-center main_container_info">
+
+    <div class="container-left px-3">
+        <h1 class="py-2">TALENT</h1>
+        <h4 class="py-2">ART BY :</h4>
+        <h4 class="py-2">WRITTEN BY :</h4>
+       
+
+    </div>
+
+    <div class="container-right px-3 ">
+       <h1 class="py-2">SPECS</h1>
+       <h4 class="py-2">SERIES : {{$comic['type']}}</h4>
+       <h4 class="py-2">PRICE : {{$comic['price']}}</h4>
+       <h4 class="py-2">ON SALE DATE : {{$comic['sale_date']}}</h4>
+    </div>
+
+</div>
+
+
+
 
 
 
